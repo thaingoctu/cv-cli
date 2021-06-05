73c7d43a4dc2705e2c4e7360d70b9645322e4000
Thai Ngoc Tu <thaingoctu@gmail.com>
2021-06-05 17:53:54 +0700

    refactor: Add helpers

---
+++ cv_cli/helpers/helpers.cpp
@@ -0,0 +1,24 @@
+#include "helpers.hpp"
+
+cv::String type2Str(int type) {
+    cv::String r;
+
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+    switch (depth) {
+        case CV_8U:  r = "CV_8U"; break;
+        case CV_8S:  r = "CV_8S"; break;
+        case CV_16U: r = "CV_16U"; break;
+        case CV_16S: r = "CV_16S"; break;
+        case CV_32S: r = "CV_32S"; break;
+        case CV_32F: r = "CV_32F"; break;
+        case CV_64F: r = "CV_64F"; break;
+        default:     r = "User"; break;
+    }
+
+    r += "C";
+    r += (chans+'0');
+
+    return r;
+}
---
+++ cv_cli/helpers/helpers.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include "opencv2/opencv.hpp"
+
+cv::String type2Str(int type);
