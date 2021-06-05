e198802e576ec940bd4ae0814ef8d7d8edae193b
Thai Ngoc Tu <thaingoctu@gmail.com>
2021-06-05 16:17:39 +0700

    feat: Add imread

---
+++ cv_cli/imread/imread.cpp
@@ -0,0 +1,138 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+constexpr auto INVALID_FLAG = (-1);
+
+static void printFlags() {
+    std::cout << "Possible flags:\n"
+        << "\tunchanged\n"
+        << "\tgrayscale\n"
+        << "\tcolor\n"
+        << "\tanydepth\n"
+        << "\tanycolor\n"
+        << "\tload_gdal\n"
+        << "\treduced_grayscale_2\n"
+        << "\treduced_grayscale_4\n"
+        << "\treduced_grayscale_8\n"
+        << "\treduced_color_2\n"
+        << "\treduced_color_4\n"
+        << "\treduced_color_8\n"
+        << "\tignore_orientation\n";
+}
+
+static int getImreadMode(const cv::String &flags) {
+    if ("unchanged" == flags) {
+        return cv::ImreadModes::IMREAD_UNCHANGED;
+    } else if ("grayscale" == flags) {
+        return cv::ImreadModes::IMREAD_GRAYSCALE;
+    } else if ("color" == flags) {
+        return cv::ImreadModes::IMREAD_COLOR;
+    } else if ("anydepth" == flags) {
+        return cv::ImreadModes::IMREAD_ANYDEPTH;
+    } else if ("anycolor" == flags) {
+        return cv::ImreadModes::IMREAD_ANYCOLOR;
+    } else if ("load_gdal" == flags) {
+        return cv::ImreadModes::IMREAD_LOAD_GDAL;
+    } else if ("reduced_grayscale_2" == flags) {
+        return cv::ImreadModes::IMREAD_REDUCED_GRAYSCALE_2;
+    } else if ("reduced_grayscale_4" == flags) {
+        return cv::ImreadModes::IMREAD_REDUCED_GRAYSCALE_4;
+    } else if ("reduced_grayscale_8" == flags) {
+        return cv::ImreadModes::IMREAD_REDUCED_GRAYSCALE_8;
+    } else if ("reduced_color_2" == flags) {
+        return cv::ImreadModes::IMREAD_REDUCED_COLOR_2;
+    } else if ("reduced_color_4" == flags) {
+        return cv::ImreadModes::IMREAD_REDUCED_COLOR_4;
+    } else if ("reduced_color_8" == flags) {
+        return cv::ImreadModes::IMREAD_REDUCED_COLOR_8;
+    } else if ("ignore_orientation" == flags) {
+        return cv::ImreadModes::IMREAD_IGNORE_ORIENTATION;
+    } else {
+        return INVALID_FLAG;
+    }
+}
+
+static cv::String type2Str(int type) {
+    cv::String r;
+
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+    switch (depth) {
+        case CV_8U:  r = "CV_8U"; break;
+        case CV_8S:  r = "CV_8S"; break;
+        case CV_16U: r = "CV_16U"; break;
+        case CV_16S: r = "CV_16S"; break;
+        case CV_32S: r = "CV_32S"; break;
+        case CV_32F: r = "CV_32F"; break;
+        case CV_64F: r = "CV_64F"; break;
+        default:     r = "User"; break;
+    }
+
+    r += "C";
+    r += (chans+'0');
+
+    return r;
+}
+
+int main(int argc, char **argv) {
+    const cv::String keys = 
+        "{ h help      |        | Display the usage                              }"
+        "{ v verbose   |        | Verbose mode                                   }"
+        "{ imshow      |        | Display an image in a window                   }"
+        "{ filestorage |        | Write data to the specified XML/YAML/JSON file }"
+        "{ @filename   | <none> | Name of file to be loaded                      }"
+        "{ flags       | color  | Mode of imread                                 }";
+
+    cv::CommandLineParser parser(argc, argv, keys);
+    parser.about("Loads an image from a file.");
+
+    if (parser.has("help")) {
+        parser.printMessage();
+        printFlags();
+        return EXIT_SUCCESS;
+    }
+
+    cv::String filename{ parser.get<cv::String>("@filename") };
+    cv::String flags{ parser.get<cv::String>("flags")};
+    cv::String filestorage{ parser.get<cv::String>("filestorage") };
+
+    if (!parser.check()) {
+        parser.printErrors();
+        return EXIT_FAILURE;
+    }
+
+    int imread_mode = getImreadMode(flags);
+
+    if (INVALID_FLAG == imread_mode) {
+        std::cerr << "[ERROR] Invalid flags.\n";
+        return EXIT_FAILURE;
+    }
+
+    cv::Mat image = cv::imread(filename, imread_mode);
+
+    if (image.empty()) {
+        std::cout << "[WARN] imread returns an empty matrix.\n";
+    }
+
+    if (parser.has("filestorage")) {
+        cv::FileStorage fs(filestorage, cv::FileStorage::WRITE);
+        fs << "mat" << image;
+    }
+
+    if (parser.has("verbose")) {
+        std::cout << "[INFO] Loaded the image:\n"
+            << "rows = " << image.rows << '\n'
+            << "cols = " << image.cols << '\n'
+            << "channels = " << image.channels() << '\n'
+            << "type = " << type2Str(image.type()) << '\n'
+            << "isContinous = " << (image.isContinuous() ? "true" : "false") << '\n';
+    }
+
+    if (!image.empty() && parser.has("imshow")) {
+        cv::imshow(filename.c_str(), image);
+        cv::waitKey();
+    }
+
+    return EXIT_SUCCESS;
+}
