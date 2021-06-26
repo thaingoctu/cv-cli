b8dd10acf9bc0359fc7dcf15cf7aabb8f1b8f93a
Thai Ngoc Tu <thaingoctu@gmail.com>
2021-06-26 16:25:48 +0700

    refactor(imread): Add ParamParser class for reusability

--- cv_cli/imread/imread.cpp
+++ cv_cli/imread/imread.cpp
@@ -2,24 +2,15 @@
 #include "helpers.hpp"
 #include <iostream>
 
-constexpr auto INVALID_FLAG{ -1 };
+const cv::String COMMON_KEYS{
+    "{ h help      |        | Display the usage                              }"
+    "{ v verbose   |        | Verbose mode                                   }"
+    "{ imshow      |        | Display an image in a window                   }"
+    "{ filestorage |        | Write data to the specified XML/YAML/JSON file }"
+    "{ @filename   | <none> | Name of file to be loaded                      }"
+};
 
-static void printFlags() {
-    std::cout << "Possible flags:\n"
-        << "\tunchanged\n"
-        << "\tgrayscale\n"
-        << "\tcolor\n"
-        << "\tanydepth\n"
-        << "\tanycolor\n"
-        << "\tload_gdal\n"
-        << "\treduced_grayscale_2\n"
-        << "\treduced_grayscale_4\n"
-        << "\treduced_grayscale_8\n"
-        << "\treduced_color_2\n"
-        << "\treduced_color_4\n"
-        << "\treduced_color_8\n"
-        << "\tignore_orientation\n";
-}
+constexpr auto INVALID_FLAG{ -1 };
 
 static int getImreadMode(const cv::String &flags) {
     if ("unchanged" == flags) {
@@ -53,53 +44,152 @@ static int getImreadMode(const cv::String &flags) {
     }
 }
 
+class ParamParser {
+public:
+    ParamParser(
+        int argc,
+        const char *const argv[],
+        const cv::String &keys = "",
+        const cv::String &about_message = "")
+        : parser_{ argc, argv, params_.append(COMMON_KEYS).append(keys) } {
+
+        parser_.about(about_message);
+        filename_.append(parser_.get<cv::String>("@filename"));
+        filestorage_.append(parser_.get<cv::String>("filestorage"));
+    }
+
+    ~ParamParser() {}
+
+    bool isParamsOK(bool print_error = true) const {
+        bool paramsOK{ parser_.check() };
+        if (!paramsOK && print_error) {
+            parser_.printErrors();
+        }
+        return paramsOK;
+    }
+
+    bool hasFileStorage() const {
+        return parser_.has("filestorage");
+    }
+
+    bool hasHelp(bool print_help = true) const {
+        bool needed_help{ parser_.has("help") };
+        if (needed_help && print_help) {
+            parser_.printMessage();
+        }
+        return needed_help;
+    }
+
+    bool hasImShow() const {
+        return parser_.has("imshow");
+    }
+
+    bool hasVerbose() const {
+        return parser_.has("verbose");
+    }
+
+    cv::String getFileName() const {
+        return filename_;
+    }
+
+    cv::String getFileStorage() const {
+        return filestorage_;
+    }
+
+protected:
+    template<typename T>
+    T getArgument(const cv::String &name) const {
+        return parser_.get<T>(name);
+    }
+
+private:
+    cv::String params_;
+    cv::String filename_;
+    cv::String filestorage_;
+    cv::CommandLineParser parser_;
+};
+
+class ImReadParamParser : public ParamParser {
+public:
+    ImReadParamParser(
+        int argc,
+        const char *const argv[],
+        const cv::String &keys = "",
+        const cv::String &about_message = "")
+        : ParamParser{ argc, argv, keys, about_message } {
+        flags_.append(getArgument<cv::String>("flags"));
+    }
+
+    ~ImReadParamParser() {}
+
+    bool hasHelp(bool print_help = true) const {
+        bool needed_help = ParamParser::hasHelp();
+        if (needed_help) {
+            printFlags();
+        }
+        return needed_help;
+    }
+
+    cv::String getFlag() const {
+        return flags_;
+    }
+
+private:
+    cv::String flags_;
+
+    void printFlags() const {
+        std::cout << "Possible flags:\n"
+            << "\tunchanged\n"
+            << "\tgrayscale\n"
+            << "\tcolor\n"
+            << "\tanydepth\n"
+            << "\tanycolor\n"
+            << "\tload_gdal\n"
+            << "\treduced_grayscale_2\n"
+            << "\treduced_grayscale_4\n"
+            << "\treduced_grayscale_8\n"
+            << "\treduced_color_2\n"
+            << "\treduced_color_4\n"
+            << "\treduced_color_8\n"
+            << "\tignore_orientation\n";
+    }
+};
+
 int main(int argc, char **argv) {
     const cv::String keys{
-        "{ h help      |        | Display the usage                              }"
-        "{ v verbose   |        | Verbose mode                                   }"
-        "{ imshow      |        | Display an image in a window                   }"
-        "{ filestorage |        | Write data to the specified XML/YAML/JSON file }"
-        "{ @filename   | <none> | Name of file to be loaded                      }"
-        "{ flags       | color  | Mode of imread                                 }"
+        "{ flags | color  | Mode of imread }"
     };
+    const cv::String about_message{ "Loads an image from a file." };
 
-    cv::CommandLineParser parser{ argc, argv, keys };
-    parser.about("Loads an image from a file.");
+    ImReadParamParser parser(argc, argv, keys, about_message);
 
-    if (parser.has("help")) {
-        parser.printMessage();
-        printFlags();
+    if (parser.hasHelp()) {
         return EXIT_SUCCESS;
     }
 
-    auto filename{ parser.get<cv::String>("@filename") };
-    auto flags{ parser.get<cv::String>("flags") };
-    auto filestorage{ parser.get<cv::String>("filestorage") };
-
-    if (!parser.check()) {
-        parser.printErrors();
+    if (!parser.isParamsOK()) {
         return EXIT_FAILURE;
     }
 
-    auto imread_mode{ getImreadMode(flags) };
+    auto imread_mode{ getImreadMode(parser.getFlag()) };
 
     if (INVALID_FLAG == imread_mode) {
         std::cerr << "[ERROR] Invalid flags.\n";
         return EXIT_FAILURE;
     }
 
-    auto image{ cv::imread(filename, imread_mode) };
+    auto image{ cv::imread(parser.getFileName(), imread_mode) };
 
     if (image.empty()) {
         std::cout << "[WARN] imread returns an empty matrix.\n";
     }
 
-    if (parser.has("filestorage")) {
-        cv::FileStorage fs{ filestorage, cv::FileStorage::WRITE };
+    if (parser.hasFileStorage()) {
+        cv::FileStorage fs{ parser.getFileStorage(), cv::FileStorage::WRITE };
         fs << "mat" << image;
     }
 
-    if (parser.has("verbose")) {
+    if (parser.hasVerbose()) {
         std::cout << "[INFO] Loaded the image:\n"
             << "rows = " << image.rows << '\n'
             << "cols = " << image.cols << '\n'
@@ -108,8 +198,8 @@ int main(int argc, char **argv) {
             << "isContinous = " << (image.isContinuous() ? "true" : "false") << '\n';
     }
 
-    if (parser.has("imshow") && !image.empty()) {
-        cv::imshow(filename, image);
+    if (parser.hasImShow() && !image.empty()) {
+        cv::imshow(parser.getFileName(), image);
         cv::waitKey();
     }
 
